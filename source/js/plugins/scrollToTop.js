56e4d144dc03a4a614facec9446f76cb15f4b0bc
ViZhe <vizhe.cat@ya.ru>
2016-09-18 16:46:40 +0300

    add js plugin scrollToTop

---
+++ source/js/plugins/scrollToTop.js
@@ -0,0 +1,31 @@
+
+import $ from 'jquery'
+
+
+const scrollToTop = (config) => {
+  const options = {
+    selector: '.js-scroll-top',
+    class: 'm-scroll-top_show',
+    ...config
+  }
+
+  const selector = $(options.selector)
+
+  $(window).on('scroll', () => {
+    const scrollTop = $(document).scrollTop()
+    if (scrollTop > 100) {
+      selector.addClass(options.class)
+    } else {
+      selector.removeClass(options.class)
+    }
+  })
+
+  selector.on('click', () => {
+    $('body, html').animate({
+      scrollTop: 0
+    }, 700)
+  })
+}
+
+
+export default scrollToTop

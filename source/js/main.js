db4775847afee0a9ef1277dac41890418747afde
ViZhe <vizhe.cat@ya.ru>
2016-12-14 23:52:34 +0300

    del lightgallery scripts

--- source/js/main.js
+++ source/js/main.js
@@ -1,16 +1,9 @@
 
 import scrollToTop from './plugins/scrollToTop'
 import './plugins/form'
-import './plugins/lightGallery'
-import './plugins/lightGalleryHash'
-import './plugins/lightGalleryVideo'
 
 
 scrollToTop()
-
-$('body').lightGallery({
-  selector: '.js-light-gallery'
-})
 
 $('body').form()
 
